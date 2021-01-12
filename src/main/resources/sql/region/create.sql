e14220d52f8b219613283b4963ad8221c82b535f
zkingboos <kingboosoficial@gmail.com>
2021-01-12 00:52:50 -0300

    updated batch sql reader for create table

--- src/main/resources/sql/region/create.sql
+++ src/main/resources/sql/region/create.sql
@@ -1,3 +1,8 @@
-CREATE TABLE IF NOT EXISTS region(
-    name varchar(255)
+create table if not exists region(
+    world_name varchar(20) not null,
+    name varchar(25) not null,
+    location_start varchar(100) not null,
+    location_end varchar(100) not null,
+    priority int not null default 1,
+    primary key(world_name, name)
 );
